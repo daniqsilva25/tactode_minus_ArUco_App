078503cbe5de407c7fb16c10c8c9c7e6fe1cea37
Daniel Queirós da Silva <daniqsilva1997@gmail.com>
2020-09-29 15:05:37 +0100

    Update with minimal changes on code

--- src/params.js
+++ src/params.js
@@ -1,17 +1,67 @@
-const hogsvmClassification = {
-  svm: 'models/HOGSVM/svm_classifier.yaml',
-  hog: 'models/HOGSVM/hog_classifier.yaml',
-  classes: 'models/HOGSVM/classes.txt',
-  size: 32
+const fs = require('fs')
+const cv = require('opencv4nodejs')
+
+// GLOBAL VARS
+const debugFolder = 'debug'
+
+// CLASSES
+class HOGSVM4detection {
+  constructor (
+    hogFile = 'dependencies/hog_detector.yaml',
+    svmFile = 'dependencies/svm_detector.yaml',
+    w = 64, h = 32,
+    hog = new cv.HOGDescriptor(),
+    svm = new cv.SVM()) {
+    this.hogFile = hogFile
+    this.svmFile = svmFile
+    this.size = {
+      width: w,
+      height: h
+    }
+    this.hog = hog
+    this.svm = svm
+  }
+
+  loadFiles () {
+    this.hog.load(this.hogFile)
+    this.svm.load(this.svmFile)
+  }
 }
 
-const hogsvmDetection = {
-  svm: 'dependencies/svm_detector.yaml',
-  hog: 'dependencies/hog_detector.yaml',
-  size: {
-    width: 64,
-    height: 32
+class HOGSVM {
+  constructor (
+    hogFile = 'models/HOGSVM/hog_classifier.yaml',
+    svmFile = 'models/HOGSVM/svm_classifier.yaml',
+    classesFile = 'models/HOGSVM/classes.txt',
+    size = 32,
+    hog = new cv.HOGDescriptor(),
+    svm = new cv.SVM()) {
+    this.hogFile = hogFile
+    this.svmFile = svmFile
+    this.classesFile = classesFile
+    this.classes = fs.readFileSync(classesFile).toString().split('\n')
+    this.size = size
+    this.hog = hog
+    this.svm = svm
   }
+
+  loadFiles () {
+    this.hog.load(this.hogFile)
+    this.svm.load(this.svmFile)
+  }
+}
+
+// FUNCTIONS
+function printResult (tiles = [], numberOfTiles = -1) {
+  const numTiles = numberOfTiles
+  let resultStr = ` Number of tiles found: ${numTiles}\n\n `
+  tiles.forEach(line => {
+    line.forEach(piece => {
+      resultStr += `| ${piece.tactode.piece} `
+    })
+    resultStr += '|\n '
+  })
+  console.log(resultStr)
 }
 
 function getCurrentTime () {
@@ -25,4 +75,4 @@ function getCurrentTime () {
   return `${dayOfMonth}/${month}/${year} ${hours}:${minutes}:${seconds}`
 }
 
-module.exports = { hogsvmClassification, hogsvmDetection, getCurrentTime }
+module.exports = { debugFolder, HOGSVM4detection, HOGSVM, printResult, getCurrentTime }
